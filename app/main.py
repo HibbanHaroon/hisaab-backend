833027c8b7dd53f4385f445c4cfaa910717c5199
HibbanHaroon <hibbanharoon@gmail.com>
2025-02-05 04:04:13 +0500

    Add routers to main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from .routers import auth, users, categories, budget, expense
 
 app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(categories.router)
+app.include_router(budget.router)
+app.include_router(expense.router)
 
 @app.get("/")
 async def root():
