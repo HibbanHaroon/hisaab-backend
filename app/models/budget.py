19f94627a93366c8c92c06a0a34415ade93c07e9
HibbanHaroon <hibbanharoon@gmail.com>
2025-02-05 11:59:18 +0500

    Add Budget model, Modify Category & Expense model

--- app/models/budget.py
+++ app/models/budget.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from ..db import Base
 
-class Expense(Base):
+class Budget(Base):
     user_id = Column(Integer, ForeignKey("user.id"))
-    cateogry_id = Column(Integer, ForeignKey("category.id"))
-    name = Column(String, index=True, nullable=False)
-    description = Column(String)
-    amount = Column(Double, nullable=False)
-    
+    category_id = Column(Integer, ForeignKey("category.id"))
+    amount_spent = Column(Double, nullable=False, default=0.0)
+    total_amount = Column(Double, nullable=False)
+
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
-    cateogry = relationship("Cateogry", back_populates="expenses")
+    # Because the name of the cateogry would also be the name of the budget, as it is a budget for that cateogry
+    category = relationship("Category", back_populates="budgets")
--- app/models/category.py
+++ app/models/category.py
@@ -1,11 +1,15 @@
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from ..db import Base
 
 class Category(Base):
-    name = Column(String, unique=True, index=True, nullable=False)
-    description = Column(String)
-    color = Column(String)
+    name = Column(String(100), unique=True, index=True, nullable=False)
+    description = Column(String(500))
+    color = Column(String(7))
     
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    expenses = relationship("Expense", back_populates="category")
+    budgets = relationship("Budget", back_populates="category")
